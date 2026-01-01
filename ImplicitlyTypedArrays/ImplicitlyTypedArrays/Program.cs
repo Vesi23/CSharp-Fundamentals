e15c84104292a76cd179f967e6d193f341757a2f
Veselina Mancheva <132705064+Vesi23@users.noreply.github.com>
2026-01-01 19:51:56 +0200

    Add files via upload

---
+++ ImplicitlyTypedArrays/ImplicitlyTypedArrays/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ImplicitlyTypedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var summerStrut= new[] { "Juice", "Missing U", "Raspberry Beret", "New York Groove", "Make Me Feel", "Rebel Rebel", "Despacito", "Los Angeles" };
+
+            //int[] ratings = 
+            var ratings= new[] { 5, 4, 4, 3, 3, 5, 5, 4 };
+
+        }
+    }
+}
