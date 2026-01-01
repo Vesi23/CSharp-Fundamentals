5225993f513597b2a764f3769b8caebe264bee33
Veselina Mancheva <132705064+Vesi23@users.noreply.github.com>
2026-01-01 19:44:53 +0200

    Add files via upload

---
+++ Building Strings/Building Strings/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrideAndPrejudice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // First string variable
+            string firstSentence = "It is a truth universally acknowledged, that a single man in possession of a good fortune must be in want of a wife.\r\n";
+            // Second string variable
+            // Second string variable
+            string firstSpeech = "My dear Mr. Bennet,\" said his lady to him one day, \"have you heard that Netherfield Park is let at last?";
+            // Print variable and newline
+            Console.WriteLine($"{firstSentence}\n {firstSpeech}");
+        }
+    }
+}
---
+++ Building Strings/Building Strings/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Building Strings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Building Strings")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c1224b0-313d-449a-b7e6-1cc331a2a165")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
