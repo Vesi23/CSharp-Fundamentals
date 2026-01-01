4de8b7b150d904bdee4c66ee286777e88c8e3c4b
Veselina Mancheva <132705064+Vesi23@users.noreply.github.com>
2026-01-01 19:48:08 +0200

    Add files via upload

---
+++ IfStatement/IfStatement/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int socks = 3;
+
+            if (socks <= 3) {
+                Console.WriteLine("Time to do laundry!");
+            }
+        }
+    }
+}
