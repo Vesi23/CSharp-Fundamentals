2129fdd11e5f8fbc72abac88c24936f5eb6b840a
Veselina Mancheva <132705064+Vesi23@users.noreply.github.com>
2026-01-01 19:49:42 +0200

    Add files via upload

---
+++ DefineAMethod/DefineAMethod/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefineAMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VisitPlanets();
+        }
+        static void VisitPlanets()
+        {
+            Console.WriteLine("My name is");
+        }
+    }
+}
