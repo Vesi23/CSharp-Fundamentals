fcf7f49664338ab7ab91acba256c95b90bc97500
Veselina Mancheva <132705064+Vesi23@users.noreply.github.com>
2026-01-01 19:42:15 +0200

    Add files via upload

---
+++ Var/Var/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var food = "apple";
+            var number = 42;
+            var isFruit = true;
+
+            Console.WriteLine($"Food: {food}, Number: {number}, IsFruit: {isFruit}");
+        }
+    }
+}
