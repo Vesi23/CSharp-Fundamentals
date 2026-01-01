2d18640061bef290c62f2fec81f36687f10df611
Veselina Mancheva <132705064+Vesi23@users.noreply.github.com>
2026-01-01 19:49:07 +0200

    Add files via upload

---
+++ TernaryOperator/TernaryOperator/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pepperLength = 4;
+            string message = (pepperLength >= 3.5) ? "ready!" : "wait a little longer";
+            message.IndexOf
+            Console.WriteLine(message);
+
+        }
+    }
+}
