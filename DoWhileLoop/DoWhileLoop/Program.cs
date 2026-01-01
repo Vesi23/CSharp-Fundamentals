61029a70107ded22e6207176ca9b7ff55c9c4f8f
Veselina Mancheva <132705064+Vesi23@users.noreply.github.com>
2026-01-01 19:54:15 +0200

    Add files via upload

---
+++ DoWhileLoop/DoWhileLoop/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool buttonClick = true;
+
+            do
+            {
+                Console.WriteLine("Alarm Ringing");
+            } while (!buttonClick);
+
+          Console.WriteLine("Time for a five minute break.");
+
+        }
+    }
+}
---
+++ DoWhileLoop/DoWhileLoop/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DoWhileLoop")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DoWhileLoop")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a70d91f7-7194-430a-a7c8-2ec3587da2d4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
