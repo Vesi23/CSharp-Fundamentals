d7287f9f5db5e5265cad524e8130d8abf1e52ca0
Veselina Mancheva <132705064+Vesi23@users.noreply.github.com>
2026-01-01 19:46:24 +0200

    Add files via upload

---
+++ Manipulate Strings/Manipulate Strings/Program.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manipulate_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Script line
+            string script = "Close on a portrait of the HANDSOME PRINCE -- as the BEAST'S giant paw slashes it.";
+
+            // Get camera directions
+            int charPosition = script.IndexOf("Close");
+            int length = "Close on".Length;
+            string cameraDirections = script.Substring(charPosition, length);
+
+            // Get scene description
+            charPosition = script.IndexOf("a portrait");
+            string sceneDescription = script.Substring(charPosition);
+
+
+            // Make camera directions uppercase
+            cameraDirections= cameraDirections.ToUpper();
+
+            // Make scene description lowercase
+            sceneDescription= sceneDescription.ToLower();
+
+            // Print results
+            Console.WriteLine(cameraDirections);
+            Console.WriteLine(sceneDescription);
+        }
+    }
+}
