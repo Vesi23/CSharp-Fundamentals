d8235cd1b3f6b9e1f44df9fb94fbd96f4ed3a2a7
Veselina Mancheva <132705064+Vesi23@users.noreply.github.com>
2026-01-01 19:46:50 +0200

    Add files via upload

---
+++ Boolean/Boolean/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanDataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //True or False: The number 500 is greater than 24.
+            bool answerOne= true;
+
+            //True or False: “coffee” contains the letter “a”.
+            bool answerTwo = false;
+
+
+        }
+    }
+}
